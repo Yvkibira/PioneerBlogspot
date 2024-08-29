b632e32f4a3536dfe5f98ebef4a00a634cf14068
Yvkibira <122603167+Yvkibira@users.noreply.github.com>
2024-08-29 11:07:00 +0300

    Create vertical-google-ad.blade.php

---
+++ resources/views/frontend/blog/vertical-google-ad.blade.php
@@ -0,0 +1,10 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2705647526062186"
+     crossorigin="anonymous"></script>
+<!-- vertical -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:160px;height:600px"
+     data-ad-client="ca-pub-2705647526062186"
+     data-ad-slot="1710327054"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
