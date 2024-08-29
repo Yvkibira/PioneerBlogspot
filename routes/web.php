ef166567aa3b84c857a20225aea4571b6c9d9b6e
Yvkibira <122603167+Yvkibira@users.noreply.github.com>
2024-08-29 11:11:33 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\PostController;
 Route::get('/', function () {
     return view('home');
 })->name('home');
-Route::get('/Blog', function () {
+Route::get('/blog', function () {
     return view('frontend.blog.home');
 })->name('blogs');
-Route::get('/Blog/{slug}', [PostController::class, 'index'])->name('blog');
-Route::get('/Blog/Categories/{category}', [PostController::class, 'category'])->name('blog-category');
+Route::get('/blog/{slug}', [PostController::class, 'index'])->name('blog');
+Route::get('/blog/categories/{category}', [PostController::class, 'category'])->name('blog-category');
